fcd933f652e3c68754d49fcd75da851d2561ee3c
Ameerpappay <ameer406023@gmail.com>
2022-05-07 08:46:53 +0530

    Add project files.

---
+++ Domain/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime UpdatedTime { get; set; }
+    }
+}
